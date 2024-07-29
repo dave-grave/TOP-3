03063ea678b05db6777aa270a35b236c902476ab
David Huang <djhuang636@gmail.com>
2024-07-28 22:04:17 -0400

    fix getHumanScore

--- script.js
+++ script.js
@@ -13,16 +13,8 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt("what do you want to play? (rock/paper/scissors): ")
-    if (choice.toUpperCase() === "ROCK") {
-        return "rock";
-    } else if (choice.toUpperCase() === "PAPER") {
-        return "paper";
-    } else if (choice.toUpperCase() === "SCISSORS") {
-        return "scissors";
-    } else {
-        return "please type a valid input.";
-    }
+    let choice = prompt("what do you want to play? (rock/paper/scissors): ");
+    return choice;
 }
 
 function playRound(humanChoice, computerChoice) {
